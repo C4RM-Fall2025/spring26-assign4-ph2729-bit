041719838c3c972a1094d6c7bd3ae6a2ce0d8154
ph2729-bit <ph2729@columbia.edu>
2026-02-21 22:37:08 -0500

    ph2729

--- BondPrice_Z_File.py
+++ BondPrice_Z_File.py
@@ -1,4 +1,13 @@
 
 
 def getBondPrice_Z(face, couponRate, times, yc):
-    return(1996533)
+    pvcfsum = 0
+    cf = face * couponRate
+    for i, y in zip(times, yc):
+        pvcf = cf * (1 + y) ** -i
+        if i == times[-1]:
+            pvcf = pvcf + face * (1 + y) ** -i
+        pvcfsum = pvcfsum + pvcf
+
+    return(pvcfsum)    
+
--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,18 @@
 
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 424, 'fizzbuzz']
-    return(v)
+    outlist = []
+    for i in range(start, finish + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            v = 'fizzbuzz'
+        elif i % 3 == 0:
+            v = 'fizz'
+        elif i % 5 == 0:
+            v = 'buzz'
+        else:
+            v = i
+        outlist.append(v)
+
+    return(outlist)
+
+
